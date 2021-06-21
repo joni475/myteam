a520e56b6fc35457296d2bd580b2ceb00b9b949c
joni475 <joni.frontend@gmail.com>
2021-06-21 17:01:45 -0500

    js. scroll error fix

--- starter-code/javascript/index.js
+++ starter-code/javascript/index.js
@@ -3,19 +3,22 @@ const hamburger = document.querySelector(".icon-hamburger");
 const closeIcon = document.querySelector(".icon-close");
 const navLink = document.querySelector(".nav-link");
 const layover = document.querySelector(".overlay");
-
+const body = document.querySelector("body");
  (function(){
    hamburger.addEventListener("click",()=>{
      layover.style.display="block";
      navLink.style.transform="translate(0)";
      closeIcon.classList.add("show");
      hamburger.style.display="none";
+     body.style.overflow = "hidden";
+
    })
    closeIcon.addEventListener("click",()=>{
     layover.style.display="none";
     navLink.style.transform="translate(120%)";
     closeIcon.classList.remove("show");
     hamburger.style.display="block";
+    body.style.overflow = "auto";
    });
     
  })();
